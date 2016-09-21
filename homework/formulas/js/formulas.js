bcbae1a551bdc8efc1b6a4b437a257eef799e5f3
smicl11 <bryan.smith094@gmail.com>
2016-09-20 19:37:39 -0700

    working C to F and F to C functions

--- homework/formulas/js/formulas.js
+++ homework/formulas/js/formulas.js
@@ -6,3 +6,26 @@
 // 3.	 Be sure to link up this file in your HTML doc
 /////////////////////////////////////////////////////////////////////
 
+//converts F to C
+function calcFahrenheitToCelcius(F) {
+  F = Math.round(F);
+  var C = ((F - 32) * 5/9);
+  C = Math.round(C);
+  return C;
+}
+
+function calcCelciusToFarenheit(C) {
+  C = Math.round(C);
+  var F = (C * (9/5) + 32);
+  F = Math.round(F);
+  return F;
+}
+
+//calculates circumference
+function calcCircumference(C) {
+
+}
+
+function calcLongSide(Height, Width) {
+
+}
