da7f964dc75cdd541c8810130e20d592215bfa10
smicl11 <bryan.smith094@gmail.com>
2016-09-28 18:34:39 -0700

    in class asignment complete

--- classwork/random-address/js/randomAddress.js
+++ classwork/random-address/js/randomAddress.js
@@ -12,7 +12,7 @@ button.addEventListener('click', generateAddress);
 // Setup
 // ------------------------------------------
 // TODO: create your arrays here (street, city, state, etc)
-var street = ["Bush", "Mission", "Valencia"];
+var street = ["225 Bush St.", "860 Mission St.", "475 Valencia"];
 var city = ["San Francisco", "Redwood City", "Palo Alto"];
 var state = ["California", "Arizona", "Nevada"];
 
@@ -21,9 +21,12 @@ var state = ["California", "Arizona", "Nevada"];
 // Event Listeners
 // ------------------------------------------
 function generateAddress(e) {
+	var randomNumber = Math.floor(Math.random() * 2);
+	console.log(street[randomNumber], city[randomNumber], state[randomNumber]);
 	// TODO: randomly select one item from each of these arrays
 	//       and then use them to construct a random address
 }
+
 
 
 // Update page functions
