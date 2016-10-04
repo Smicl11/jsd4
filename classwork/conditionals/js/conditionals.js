4de2558f26bca235241bc1a2dedf914d01a7f365
smicl11 <bryan.smith094@gmail.com>
2016-10-03 19:40:35 -0700

    age-check in class lab

--- classwork/conditionals/js/conditionals.js
+++ classwork/conditionals/js/conditionals.js
@@ -1,1 +1,22 @@
 console.log("hello world");
+
+//if statements
+var grade = prompt("What is your Grade?");
+
+if (grade == "A") {
+  console.log("Awesome Job!");
+} else {
+  console.log("if: do better!");
+}
+
+//switch statements (you compare one value against a list of options)
+switch(grade) {
+  case 'A':
+    console.log("Awesome Job!");
+    break;
+  case 'B':
+    console.log("do better!");
+    break;
+  default:
+    console.log("Case: Get your life together!");
+}
