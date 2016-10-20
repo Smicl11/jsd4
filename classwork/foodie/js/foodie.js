fa87710eb70ceebea865eb2702ea0a526d38e3c4
smicl11 <bryan.smith094@gmail.com>
2016-10-19 20:51:04 -0700

    added foodie-things classwork starter code

---
+++ classwork/foodie/js/foodie.js
@@ -0,0 +1,18 @@
+// Elements
+// ------------------------------------
+var form = document.querySelector("form");
+var zip = document.querySelector("form .zip");
+var results = document.querySelector(".results");
+
+
+// Event
+// ------------------------------------
+form.addEventListener('submit', getRestaurants);
+
+
+// Event Handler 
+// ------------------------------------
+
+
+// Update page
+// ------------------------------------
