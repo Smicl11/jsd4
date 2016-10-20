62c312d0540110864527585dc87de93be142d015
smicl11 <bryan.smith094@gmail.com>
2016-10-19 22:02:37 -0700

    classwork example

--- classwork/foodie/js/foodie.js
+++ classwork/foodie/js/foodie.js
@@ -10,9 +10,30 @@ var results = document.querySelector(".results");
 form.addEventListener('submit', getRestaurants);
 
 
-// Event Handler 
+// Event Handler
 // ------------------------------------
+function getRestaurants(event) {
+  event.preventDefault();
+  var search = zip.value;
+  var url = "http://opentable.herokuapp.com/api/restaurants?zip=" + search;
+
+  jQuery.getJSON(url, updateRestaurants);
+}
+
+// $('form .zip').on('submit', function(e) {
+//   e.preventDefault();
+//   console.log(zip.value);
+// });
 
 
 // Update page
 // ------------------------------------
+function updateRestaurants(json) {
+  // console.log("updateRestaurants", json);
+  json.restaurants.forEach(createRestaurants);
+}
+
+function createRestaurants(restaurant) {
+  console.log(restaurant.name);
+  
+}
